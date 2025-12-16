2ef7d35820b2a1b0bd55232a13e24a3bc5f723ce
erislaniojaco-lab <erislanio.jaco@aluno.ufca.edu.br>
2025-12-15 22:29:42 -0300

    Update Perfil_professor.py

--- Perfil_professor.py
+++ Perfil_professor.py
@@ -1,16 +1,11 @@
-# Importa o módulo json para manipulação de arquivos no formato JSON
-import json
+import json # Importa o módulo json
 
-# Importa a função responsável pelo registro de frequência dos alunos
-from Frequencia import iniciar_frequencia
+from Frequencia import iniciar_frequencia # Importa a função 
 
-# Importa a função responsável pelo registro de notas dos alunos
-from Nota import registrar_nota
+from Nota import registrar_nota # Importa a função responsável pelo registro de notas dos alunos
 
-# Define o caminho do arquivo JSON que armazena os dados das turmas
-arquivo_turma_json = "dados/turma.json"
+arquivo_turma_json = "dados/turma.json" # Define o caminho do arquivo JSON que armazena os dados das turmas
 
-# Define o caminho do arquivo JSON que armazena os dados dos alunos
 arquivo_aluno_json = "dados/aluno.json"
 
 # Lista global utilizada para armazenar os alunos
@@ -34,20 +29,16 @@ def filtrar_aluno(turma):
         if item.get('turma') == turma:
             lista_filtro_aluno.append(item)
 
-    # Exibe os alunos da turma selecionada
     print("\n")
     print(f"ALUNOS DA TURMA: {turma}")
     for item in lista_filtro_aluno:
         print(f"matricula = {item['matricula']} --- {item['nome']}")
 
-
-# Lista global utilizada para armazenar as turmas cadastradas
 lista_turma = []
 
 
 # Função responsável por listar todas as turmas cadastradas no sistema
 def listar_turma():
-    # Indica que a variável lista_turma será utilizada no escopo global
     global lista_turma
 
     # Abre o arquivo JSON contendo os dados das turmas
@@ -93,16 +84,14 @@ Selecione a opção desejada:
 5 → Encerrar sessão
     """))
 
-        # Verifica se a opção selecionada corresponde ao registro de frequência
         if opcao == 1:
             print("\n")
             iniciar_frequencia(turma, lista_filtro_aluno)
 
-        # Verifica se a opção selecionada corresponde ao registro de notas
         if opcao == 2:
             print("\n")
             registrar_nota(nome_usuario, turma, lista_filtro_aluno)
 
-        # Encerra o menu quando o professor seleciona a opção de saída
         if opcao == 5:
+
             break
